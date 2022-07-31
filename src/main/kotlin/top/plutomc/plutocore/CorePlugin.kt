e5826514c1003a1700bc22cfb00d3ea4bdb8290b
GerryYuu <jjjooo233@163.com>
2022-07-31 19:41:20 +0800

    修改结构

--- src/main/kotlin/top/plutomc/plutocore/CorePlugin.kt
+++ src/main/kotlin/top/plutomc/plutocore/CorePlugin.kt
@@ -2,6 +2,8 @@ package top.plutomc.plutocore
 
 import net.kyori.adventure.platform.bukkit.BukkitAudiences
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.scheduler.BukkitRunnable
+import org.bukkit.scheduler.BukkitTask
 import top.plutomc.plutocore.listeners.PlayerListener
 import java.io.File
 
@@ -20,6 +22,14 @@ class CorePlugin : JavaPlugin(){
         val file = File(dataFolder, "config.yml")
         if (file.exists().not()) saveDefaultConfig()
         server.pluginManager.registerEvents(PlayerListener(), this)
+        val tabListHeaderTask = object : BukkitRunnable(){
+            override fun run() {
+                val stringBuilder = StringBuilder()
+                val list = config.getStringList("tablist.header")
+                list.forEach { stringBuilder.append(it).append("<br>") }
+
+            }
+        }.runTaskTimerAsynchronously(this, 0L, 20L)
         logger.info("Done.")
     }
 
--- src/main/kotlin/top/plutomc/plutocore/utils/TablistUtil.kt
+++ src/main/kotlin/top/plutomc/plutocore/utils/TablistUtil.kt
@@ -1,29 +1,31 @@
 package top.plutomc.plutocore.utils
 
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.minimessage.MiniMessage
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver
 import org.bukkit.entity.Player
 import top.plutomc.plutocore.CorePlugin
 
 object TablistUtil {
-    fun updateHeader(component: Component) {
+    fun updateHeader(content: String?) {
         CorePlugin.instance()!!.server.onlinePlayers.forEach {
-            updateHeaderFor(it, component)
+            updateHeaderFor(it, content)
         }
     }
 
-    private fun updateHeaderFor(player: Player, component: Component) {
+    private fun updateHeaderFor(player: Player, content: String?, vararg tagResolver: TagResolver) {
         val playerAudience = CorePlugin.bukkitAudiences()!!.player(player)
-        playerAudience.sendPlayerListHeader(component)
+        content?.let { MiniMessage.miniMessage().deserialize(it, *tagResolver) }?.let { playerAudience.sendPlayerListHeader(it) }
     }
 
-    fun updateFooter(component: Component) {
+    fun updateFooter(content: String?) {
         CorePlugin.instance()!!.server.onlinePlayers.forEach {
-            updateFooterFor(it, component)
+            updateFooterFor(it, content)
         }
     }
 
-    private fun updateFooterFor(player: Player, component: Component) {
+    private fun updateFooterFor(player: Player, content: String?, vararg tagResolver: TagResolver) {
         val playerAudience = CorePlugin.bukkitAudiences()!!.player(player)
-        playerAudience.sendPlayerListFooter(component)
+        content?.let { MiniMessage.miniMessage().deserialize(it, *tagResolver) }?.let { playerAudience.sendPlayerListFooter(it) }
     }
 }
