9450725a87111881792aae7c0c1edbcb573ba80b
SadlyGuy <53771072+GerryYuu@users.noreply.github.com>
2022-08-04 18:02:09 +0800

    Update LinkParser.java

--- src/main/java/top/plutomc/plutocore/utils/LinkParser.java
+++ src/main/java/top/plutomc/plutocore/utils/LinkParser.java
@@ -7,7 +7,7 @@ public class LinkParser {
 
     public static String parseUrl(String text, String hover) {
         String urlLinkRegex = "[(http(s)?):\\/\\/(www\\.)?a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)";
-        String replacement = "<hover:show_text:'{SPECIAL|||!@#$%^&*()|||SPECIAL}'><click:open_url:'$0'><aqua><u>$0</u></aqua></click></hover>".replace("{SPECIAL|||!@#$%^&*()|||SPECIAL}", hover);
+        String replacement = "<hover:show_text:'" + hover + "'><click:open_url:'$0'><aqua><u>$0</u></aqua></click></hover>".replace("{SPECIAL|||!@#$%^&*()|||SPECIAL}", hover);
         return text.replaceAll(urlLinkRegex, replacement);
     }
 }
